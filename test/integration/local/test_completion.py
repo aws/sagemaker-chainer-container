72f1a79461e16d06fd708cc53087d96af737dca5
Andre Moeller <amoeller@amazon.com>
2018-05-01 02:55:43 -0700

    combine failure / process finishing tests

--- test/integration/local/test_completion.py
+++ test/integration/local/test_completion.py
@@ -1,11 +1,8 @@
 import os
 
-import pytest
-
 from test.utils import local_mode
 
-resource_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'resources')
-
+resource_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'resources', 'completion')
 
 
 def test_all_processes_finish_with_mpi(docker_image, opt_ml, use_gpu):
@@ -23,7 +20,6 @@ def test_all_processes_finish_with_mpi(docker_image, opt_ml, use_gpu):
                      hyperparameters=hyperparameters, source_dir=resource_path, use_gpu=use_gpu,
                      cluster_size=cluster_size)
 
-
     assert local_mode.file_exists(opt_ml, 'output/data/algo-2/process_could_complete', host='algo-2'), \
         'Model was not saved'
 
@@ -38,6 +34,30 @@ def test_training_jobs_do_not_stall(docker_image, opt_ml, use_gpu):
     hyperparameters = {'use_mpi': True,
                        'process_slots_per_host': 1,
                        'num_processes': 2}
+
+    local_mode.train(customer_script, resource_path, docker_image, opt_ml,
+                     hyperparameters=hyperparameters, source_dir=resource_path, use_gpu=use_gpu,
+                     cluster_size=cluster_size)
+
+    assert local_mode.file_exists(opt_ml, 'output/failure'), 'Failure did not happen'
+
+
+def test_single_machine_failure(docker_image, opt_ml, use_gpu):
+    customer_script = 'failure_script.py'
+
+    local_mode.train(customer_script, resource_path, docker_image, opt_ml, source_dir=resource_path,
+                     use_gpu=use_gpu)
+
+    assert local_mode.file_exists(opt_ml, 'output/failure'), 'Failure did not happen'
+
+
+def test_distributed_failure(docker_image, opt_ml, use_gpu):
+    customer_script = 'failure_script.py'
+    cluster_size = 2
+    hyperparameters = {'rank': 'inter_rank',
+                       'process_slots_per_host': 1,
+                       'num_processes': cluster_size,
+                       'node_to_fail': 1}
 
     local_mode.train(customer_script, resource_path, docker_image, opt_ml,
                      hyperparameters=hyperparameters, source_dir=resource_path, use_gpu=use_gpu,
