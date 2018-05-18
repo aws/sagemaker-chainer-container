f7581f9076b776dba4bf698aea35b5596f8c1fc1
Marcio Dos Santos <mvs@amazon.com>
2018-05-18 13:23:52 -0700

    Dynamic mpi script

--- src/chainer_framework/training.py
+++ src/chainer_framework/training.py
@@ -14,8 +14,10 @@ import logging
 import os
 import shlex
 import socket
+import stat
 import subprocess
 import sys
+import textwrap
 import time
 import traceback
 
@@ -70,11 +72,10 @@ def train(training_env):
         _change_hostname(current_host)
         _start_ssh_daemon()
 
-        replace_mpi_command(training_env)
+        _create_mpi_script(training_env)
 
         if current_host == _get_master_host_name(hosts):
             _wait_for_worker_nodes_to_start_sshd(hosts)
-
 
             _run_mpi_on_all_nodes(training_env)
         else:
@@ -111,7 +112,15 @@ def _get_master_host_name(hosts):
 def _run_mpi_on_all_nodes(training_env):
     mpi_command = _get_mpi_command(training_env)
     logger.info("mpi_command: %s", mpi_command)
-    subprocess.check_call(shlex.split(mpi_command))
+    try:
+        subprocess.check_call(shlex.split(mpi_command))
+
+        write_success_file(training_env.output_dir)
+    except Exception as e:
+        trc = traceback.format_exc()
+        message = 'uncaught exception during training: {}\n{}\n'.format(e, trc)
+        logger.error(message)
+        write_failure_file(message, training_env.output_dir)
 
 
 def _get_mpi_command(training_env):
@@ -190,38 +199,35 @@ def _get_mpi_command(training_env):
 
     mpi_command += " {} ".format(additional_mpi_options) + " {}".format(_MPI_SCRIPT)
 
-
     return mpi_command
 
 
-def replace_mpi_command(training_env):
-    with open(_MPI_SCRIPT) as o:
-        x = o.read()
+def _create_mpi_script(training_env):
     hyperparameters = mapping.to_cmd_args(training_env.hyperparameters)
     channels = mapping.to_cmd_args(training_env.channel_input_dirs)
     modules.download_and_install(training_env.module_dir)
+
     python_cmd = [sys.executable, '-m', training_env.module_name]
     python_cmd.extend(hyperparameters)
     python_cmd.extend(channels)
-    print("COMMAND YA===========================================================")
-    print(python_cmd)
-    print(hyperparameters)
-    print(channels)
-    x = x.replace('PLACEHOLDER_FOR_SCRIPT_MODE', ' '.join(python_cmd))
-    with open(_MPI_SCRIPT, 'w') as w:
-        w.write(x)
-    with open(_MPI_SCRIPT,) as o:
-        t = o.read()
 
-    import os
-    import stat
+    content = textwrap.dedent("""#!/usr/bin/env bash
+# For distributed training: the 'master node' runs mpirun with this script, '/mpi_script.sh'
+# This script creates a file '/mpi_is_running' that worker nodes use to determine whether training (started by MPI from
+# the master node) is still running. Processes on worker nodes use /mpi_is_finished file to determine when to exit.
+touch /mpi_is_running
+%s
+EXIT_CODE=$?
+touch /mpi_is_finished
+
+exit ${EXIT_CODE}
+""" % ' '.join(python_cmd))
+
+    with open(_MPI_SCRIPT, 'w') as w:
+        w.write(content)
 
     st = os.stat(_MPI_SCRIPT)
     os.chmod(_MPI_SCRIPT, st.st_mode | stat.S_IEXEC)
-
-    # python -m mpi4py -m chainer_framework.training
-    print("COMMAND YO===========================================================")
-    print(t)
 
 
 def _start_ssh_daemon():
@@ -231,7 +237,7 @@ def _start_ssh_daemon():
 def _wait_for_training_to_finish(training_env):
     current_host = training_env.current_host
 
-    logger.info("worker node {} is waiting for MPI to start training process %s", current_host)
+    logger.info("worker node %s is waiting for MPI to start training process", current_host)
     _wait_for_mpi_to_start_running()
 
     logger.info("MPI started training process on worker node %s", current_host)
