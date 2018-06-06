72b91b0cf0e0a8068ce72b835125016755effea9
andremoeller <andremoeller@users.noreply.github.com>
2018-06-06 00:12:25 -0700

    remove preprod from test fixture (#43)

--- test/conftest.py
+++ test/conftest.py
@@ -122,7 +122,7 @@ def fixture_docker_registry(aws_id, region):
 
 @pytest.fixture(scope='session', name='ecr_image')
 def fixture_ecr_image(docker_registry, docker_base_name, tag):
-    return '{}/preprod-{}:{}'.format(docker_registry, docker_base_name, tag)
+    return '{}/{}:{}'.format(docker_registry, docker_base_name, tag)
 
 
 @pytest.fixture(scope='session', name='sagemaker_session')
