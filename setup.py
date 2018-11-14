be1dde69abbb871e91c0f82cc86e85b3fb4842c2
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2018-11-14 10:08:44 -0800

    Unfreeze requests version (#60)
    The recent Python SDK release fixed a previous dependency conflict,
    which is why we needed to freeze the requests version in the first place.

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
         'test': [
             'tox', 'flake8', 'coverage', 'flake8-import-order', 'pytest', 'pytest-cov',
             'pytest-xdist', 'mock', 'Flask', 'boto3>=1.4.8', 'docker-compose',
-            'nvidia-docker-compose', 'sagemaker>=1.3.0', 'PyYAML', 'requests==2.18.4'
+            'nvidia-docker-compose', 'sagemaker>=1.3.0', 'PyYAML', 'requests',
         ]
     })
