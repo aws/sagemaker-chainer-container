053fe15e55e2d37e99a919799f1017c6d1135cac
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-04-10 10:48:28 -0700

    Use the SageMaker Python SDK for failure scenario local integ tests

--- test/integration/local/test_failure_scenarios.py
+++ test/integration/local/test_failure_scenarios.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2017-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You
 # may not use this file except in compliance with the License. A copy of
@@ -14,65 +14,107 @@ from __future__ import absolute_import
 
 import os
 
-from test.utils import local_mode
+import pytest
+from sagemaker.chainer import Chainer
+
+from test.utils import test_utils
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 resource_path = os.path.join(current_dir, '..', '..', 'resources', 'failure_scenarios')
 
 
-def test_all_processes_finish_with_mpi(docker_image, opt_ml, use_gpu):
+def test_all_processes_finish_with_mpi(docker_image, sagemaker_local_session, tmpdir):
     """
     This test validates that all training processes finish before containers are shut down.
     """
     customer_script = 'all_processes_finish_customer_script.py'
-
-    cluster_size = 2
     hyperparameters = {'sagemaker_use_mpi': True, 'sagemaker_process_slots_per_host': 2,
                        'sagemaker_num_processes': 4}
 
-    local_mode.train(customer_script, resource_path, docker_image, opt_ml,
-                     hyperparameters=hyperparameters, source_dir=resource_path, use_gpu=use_gpu,
-                     cluster_size=cluster_size)
+    estimator = Chainer(entry_point=customer_script,
+                        source_dir=resource_path,
+                        role='SageMakerRole',
+                        image_name=docker_image,
+                        train_instance_count=2,
+                        train_instance_type='local',
+                        sagemaker_session=sagemaker_local_session,
+                        hyperparameters=hyperparameters,
+                        output_path='file://{}'.format(tmpdir))
 
-    file_name = 'output/data/process_could_complete'
+    estimator.fit()
 
-    assert local_mode.file_exists(opt_ml, file_name, host='algo-2'), 'Model was not saved'
+    completion_file = {'output': [os.path.join('data', 'algo-2', 'process_could_complete')]}
+    test_utils.files_exist(str(tmpdir), completion_file)
 
 
-def test_training_jobs_do_not_stall(docker_image, opt_ml, use_gpu):
+def test_training_jobs_do_not_stall(docker_image, sagemaker_local_session, tmpdir):
     """
     This test validates that training does not stall.
     https://github.com/chainer/chainermn/issues/236
     """
     customer_script = 'training_jobs_do_not_stall_customer_script.py'
-    cluster_size = 2
     hyperparameters = {'sagemaker_use_mpi': True, 'sagemaker_process_slots_per_host': 1,
                        'sagemaker_num_processes': 2}
 
-    local_mode.train(customer_script, resource_path, docker_image, opt_ml,
-                     hyperparameters=hyperparameters, source_dir=resource_path,
-                     use_gpu=use_gpu, cluster_size=cluster_size)
+    estimator = Chainer(entry_point=customer_script,
+                        source_dir=resource_path,
+                        role='SageMakerRole',
+                        image_name=docker_image,
+                        train_instance_count=2,
+                        train_instance_type='local',
+                        sagemaker_session=sagemaker_local_session,
+                        hyperparameters=hyperparameters,
+                        output_path='file://{}'.format(tmpdir))
 
-    assert local_mode.file_exists(opt_ml, 'output/failure'), 'Failure did not happen'
+    # Local Mode doesn't export model/output artifacts upon failure
+    # https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/local/image.py#L133-L141
+    with pytest.raises(RuntimeError) as e:
+        estimator.fit()
+
+    assert 'Failed to run:' in str(e)
+    assert 'Process exited with code: 1' in str(e)
 
 
-def test_single_machine_failure(docker_image, opt_ml, use_gpu):
+def test_single_machine_failure(docker_image, instance_type, sagemaker_local_session, tmpdir):
     customer_script = 'failure_script.py'
+    estimator = Chainer(entry_point=customer_script,
+                        source_dir=resource_path,
+                        role='SageMakerRole',
+                        image_name=docker_image,
+                        train_instance_count=1,
+                        train_instance_type=instance_type,
+                        sagemaker_session=sagemaker_local_session,
+                        output_path='file://{}'.format(tmpdir))
 
-    local_mode.train(customer_script, resource_path, docker_image, opt_ml,
-                     source_dir=resource_path, use_gpu=use_gpu)
+    # Local Mode doesn't export model/output artifacts upon failure
+    # https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/local/image.py#L133-L141
+    with pytest.raises(RuntimeError) as e:
+        estimator.fit()
 
-    assert local_mode.file_exists(opt_ml, 'output/failure'), 'Failure did not happen'
+    assert 'Failed to run:' in str(e)
+    assert 'Process exited with code: 1' in str(e)
 
 
-def test_distributed_failure(docker_image, opt_ml, use_gpu):
+def test_distributed_failure(docker_image, sagemaker_local_session, tmpdir):
     customer_script = 'failure_script.py'
     cluster_size = 2
     hyperparameters = {'sagemaker_process_slots_per_host': 1,
                        'sagemaker_num_processes': cluster_size, 'node_to_fail': 1}
 
-    local_mode.train(customer_script, resource_path, docker_image, opt_ml,
-                     hyperparameters=hyperparameters, source_dir=resource_path,
-                     use_gpu=use_gpu, cluster_size=cluster_size)
+    estimator = Chainer(entry_point=customer_script,
+                        source_dir=resource_path,
+                        role='SageMakerRole',
+                        image_name=docker_image,
+                        train_instance_count=cluster_size,
+                        train_instance_type='local',
+                        sagemaker_session=sagemaker_local_session,
+                        hyperparameters=hyperparameters,
+                        output_path='file://{}'.format(tmpdir))
 
-    assert local_mode.file_exists(opt_ml, 'output/failure'), 'Failure did not happen'
+    # Local Mode doesn't export model/output artifacts upon failure
+    # https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/local/image.py#L133-L141
+    with pytest.raises(RuntimeError) as e:
+        estimator.fit()
+
+    assert 'Failed to run:' in str(e)
+    assert 'Process exited with code: 1' in str(e)
