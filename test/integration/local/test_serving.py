4a89af2d9ec3d57a54223cfb8ae6f2ebb765d2cc
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-04-11 08:39:05 -0700

    Use the SageMaker Python SDK for local serving integ test

--- test/integration/local/test_serving.py
+++ test/integration/local/test_serving.py
@@ -1,21 +1,45 @@
+# Copyright 2017-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
 from __future__ import absolute_import
 
 import os
 
-import requests
+from sagemaker.chainer import ChainerModel
+from sagemaker.predictor import BytesDeserializer
 
-from test.utils import local_mode
+from test.utils import test_utils
 
 path = os.path.dirname(os.path.realpath(__file__))
-resources_path = os.path.abspath(os.path.join(path, '..', '..', 'resources'))
+resources_path = os.path.abspath(os.path.join(path, '..', '..', 'resources', 'serving'))
 
 
 def test_serving_calls_model_fn_once(docker_image, opt_ml):
     script_path = os.path.join(resources_path, 'call_model_fn_once.py')
-    with local_mode.serve(script_path, model_dir=None, image_name=docker_image, opt_ml=opt_ml,
-                          additional_env_vars=['SAGEMAKER_MODEL_SERVER_WORKERS=2']):
+    model_path = 'file://{}'.format(os.path.join(resources_path, 'model.tar.gz'))
 
-        # call enough times to ensure multiple requests to a worker
-        for i in range(3):
-            # will return 500 error if model_fn called during request handling
-            assert b'output' == requests.post(local_mode.REQUEST_URL, data=b'input').content
+    model = ChainerModel(model_path, 'SageMakerRole', script_path, image=docker_image,
+                         model_server_workers=2)
+
+    with test_utils.local_mode_lock():
+        try:
+            predictor = model.deploy(1, 'local')
+            predictor.accept = None
+            predictor.deserializer = BytesDeserializer()
+
+            # call enough times to ensure multiple requests to a worker
+            for i in range(3):
+                # will return 500 error if model_fn called during request handling
+                response = predictor.predict(b'input')
+                assert response == b'output'
+        finally:
+            predictor.delete_endpoint()
