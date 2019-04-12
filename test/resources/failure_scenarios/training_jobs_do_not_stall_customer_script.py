4184a1e57e49561e3b245dbb4ba687c7392940d0
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-04-12 14:39:57 -0700

    Use the SageMaker Python SDK for failure scenario local integ tests (#76)

--- test/resources/failure_scenarios/training_jobs_do_not_stall_customer_script.py
+++ test/resources/failure_scenarios/training_jobs_do_not_stall_customer_script.py
@@ -12,13 +12,18 @@
 # language governing permissions and limitations under the License.
 from __future__ import absolute_import
 
+import os
+
 import chainermn
 import mpi4py.MPI
+import sagemaker_containers
 
 
 if __name__ == '__main__':
     mpi_comm = mpi4py.MPI.COMM_WORLD
 
     if mpi_comm.rank == 0:
+        env = sagemaker_containers.training_env()
+        open(os.path.join(env.output_data_dir, 'this_file_is_expected'), 'a').close()
         raise ValueError('failure!')
     chainermn.create_communicator('naive', mpi_comm)
