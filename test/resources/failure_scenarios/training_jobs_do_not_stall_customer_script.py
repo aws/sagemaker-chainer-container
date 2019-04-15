003013af17d88fd095ea673c4eb859e9017eeb69
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-04-15 14:07:25 -0700

    bump sleep time

--- test/resources/failure_scenarios/training_jobs_do_not_stall_customer_script.py
+++ test/resources/failure_scenarios/training_jobs_do_not_stall_customer_script.py
@@ -30,5 +30,5 @@ if __name__ == '__main__':
     else:
         # Local Mode can lead to a false positive if the successful container exits first
         # TODO: remove this when Local Mode handles these types of failures correctly
-        time.sleep(4)
+        time.sleep(2)
     chainermn.create_communicator('naive', mpi_comm)
