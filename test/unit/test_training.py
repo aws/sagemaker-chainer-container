16e7badbe22400b808e604721e3e3265e376e523
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-05-01 15:46:04 -0700

    fix unit tests

--- test/unit/test_training.py
+++ test/unit/test_training.py
@@ -35,28 +35,33 @@ def mock_training_env(current_host='algo-1', hosts=None, hyperparameters=None,
                      **kwargs)
 
 
+@patch('sagemaker_containers.beta.framework.modules.download_and_install')
 @patch('sagemaker_containers.beta.framework.entry_point.run')
-def test_single_machine(run_entry):
+def test_single_machine(run_entry, download_and_install):
     env = mock_training_env()
     training.train(env, {})
 
+    download_and_install.assert_called_with('s3://my/script')
     run_entry.assert_called_with('s3://my/script',
-                                 's3://my/script',
+                                 'imagenet',
                                  env.to_cmd_args(),
                                  env.to_env_vars(),
                                  runner=framework.runner.ProcessRunnerType,
                                  extra_opts={})
 
 
+@patch('sagemaker_containers.beta.framework.modules.download_and_install')
 @patch('sagemaker_containers.beta.framework.entry_point.run')
-def test_distributed_training(run_entry):
+def test_distributed_training(run_entry, download_and_install):
     hosts = ['algo-1', 'algo-2']
     env = mock_training_env(hosts=hosts)
     training.train(env, {})
 
+    download_and_install.assert_called_with('s3://my/script')
     run_entry.assert_called_with('s3://my/script',
-                                 's3://my/script',
+                                 'imagenet',
                                  env.to_cmd_args(),
                                  env.to_env_vars(),
                                  runner=framework.runner.MPIRunnerType,
-                                 extra_opts={})
+                                 extra_opts={'sagemaker_mpi_num_of_processes_per_host': None,
+                                             'sagemaker_mpi_num_processes': None})
