7b0f1b1f62100a21969c33f4bcdaeaa8d45769f6
Dan Choi <choidan@amazon.com>
2019-05-28 17:36:25 -0700

    pin pluggy version to workaround configparser error

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     dependency_links=['pip install git+https://github.com/aws/sagemaker-python-sdk-staging'],
     extras_require={
         'test': [
-            'tox', 'flake8', 'coverage', 'flake8-import-order', 'pytest', 'pytest-cov',
+            'tox', 'pluggy==0.11', 'flake8', 'coverage', 'flake8-import-order', 'pytest', 'pytest-cov',
             'pytest-xdist', 'mock', 'Flask', 'boto3>=1.4.8', 'docker-compose',
             'nvidia-docker-compose', 'sagemaker>=1.18.14', 'PyYAML==3.10'
         ]
