5ac2ffd132dda0c0e071f5aef8987e615ead857d
ci <ci>
2019-08-21 13:04:08 +0000

    split sagemaker test to training and deploy

--- test/integration/sagemaker/test_mnist.py
+++ test/integration/sagemaker/test_mnist.py
@@ -16,21 +16,29 @@ import os
 
 import numpy as np
 import pytest
+import sagemaker.utils
 from sagemaker.chainer import Chainer
+from sagemaker.chainer.model import ChainerModel
 from sagemaker.utils import sagemaker_timestamp
 
-from timeout import timeout, timeout_and_delete_endpoint
+from timeout import timeout, timeout_and_delete_endpoint_by_name
 
-
-def test_chainer_mnist_single_machine(sagemaker_session, ecr_image, instance_type):
-    _test_mnist(sagemaker_session, ecr_image, instance_type, 1, 'single_machine_customer_script.py')
 
 @pytest.mark.deploy_test
-def test_chainer_mnist_distributed(sagemaker_session, ecr_image, instance_type):
-    _test_mnist(sagemaker_session, ecr_image, instance_type, 2, 'distributed_customer_script.py')
+def test_chainer_mnist_single_machine_train(sagemaker_session, ecr_image, instance_type):
+    _test_mnist_train(sagemaker_session, ecr_image, instance_type, 1, 'single_machine_customer_script.py')
 
 
-def _test_mnist(sagemaker_session, ecr_image, instance_type, instance_count, script):
+@pytest.mark.deploy_test
+def test_chainer_deploy(sagemaker_session, instance_type):
+    _test_mnist_deploy(sagemaker_session, instance_type)
+
+
+def test_chainer_mnist_distributed_train(sagemaker_session, ecr_image, instance_type):
+    _test_mnist_train(sagemaker_session, ecr_image, instance_type, 2, 'distributed_customer_script.py')
+
+
+def _test_mnist_train(sagemaker_session, ecr_image, instance_type, instance_count, script):
     source_dir = 'test/resources/mnist'
 
     with timeout(minutes=15):
@@ -57,7 +65,23 @@ def _test_mnist(sagemaker_session, ecr_image, instance_type, instance_count, scr
 
         chainer.fit({'train': train_input, 'test': test_input})
 
-    with timeout_and_delete_endpoint(estimator=chainer, minutes=30):
+
+def _test_mnist_deploy(sagemaker_session, instance_type):
+    model_path = 'test/resources/mnist/model.tar.gz'
+    script_path = 'test/resources/mnist/mnist.py'
+
+    endpoint_name = sagemaker.utils.unique_name_from_base('sagemaker-chainer-test')
+    model_data = sagemaker_session.upload_data(
+        path=model_path,
+        key_prefix='sagemaker-chainer/models',
+    )
+
+    with timeout_and_delete_endpoint_by_name(endpoint_name, sagemaker_session, minutes=30):
+        chainer = ChainerModel(
+            model_data=model_data,
+            role='SageMakerRole',
+            entry_point=script_path,
+        )
         predictor = chainer.deploy(initial_instance_count=1, instance_type=instance_type)
 
         batch_size = 100
